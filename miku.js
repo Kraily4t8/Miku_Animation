275f7a1dd92e954cbf32d5b6189c508caf183135
Kraily4t8 <53635947+Kraily4t8@users.noreply.github.com>
2024-01-05 01:54:08 -0800

    increased size and added moveScale

--- miku.js
+++ miku.js
@@ -5,11 +5,12 @@ class Miku {
         this.idle = new Animator(this.spritesheet, 2, 1, 56, 64, 10, 0.2, 3, false, true);
         this.walkRight = new Animator(this.spritesheet, 2, 65, 75, 67, 8, 0.2, 2, false, true);
 
-        this.jump = new Animator(this.spritesheet, 5, 134, 75, 100, 9, 0.28, 2.25, false, true);
+        this.jump = new Animator(this.spritesheet, 5, 134, 75, 100, 9, 0.23, 2.25, false, true);
 
         this.tick = 0;
         this.xPos = 400;
         this.yPos = 250;
+        this.moveScale = 1.5;
         //loop length
     }
 
@@ -17,30 +18,30 @@ class Miku {
         // determine amount of time of the start up jump
         console.log(this.jump.currentFrame());
         this.tick++;
-        if(this.xPos > 980) this.xPos = -200;
+        if(this.xPos > 960) this.xPos = -240;
         if(this.tick > 2) {
             switch(this.jump.currentFrame()) {
-                case 1: this.yPos -=2;
-                        this.xPos +=2;
+                case 1: this.yPos -=2 * this.moveScale;
+                    this.xPos +=2 * this.moveScale;
                     break;
-                case 2: this.yPos -=2;
-                this.xPos +=2.5;
+                case 2: this.yPos -=2 * this.moveScale;
+                    this.xPos +=2.5 * this.moveScale;
                     break;
-                case 3: this.yPos -=2;
-                this.xPos +=2.5;
+                case 3: this.yPos -=2 * this.moveScale;
+                    this.xPos +=2.5 * this.moveScale;
                     break;
-                case 4: this.yPos -=0;
-                this.xPos +=2.5;
+                case 4: this.yPos -=0 * this.moveScale;
+                    this.xPos +=2.5 * this.moveScale;
                     break;
-                case 5: this.yPos +=3;
-                this.xPos +=2.5;
+                case 5: this.yPos +=3 * this.moveScale;
+                    this.xPos +=2.5 * this.moveScale;
                     break;
-                case 6: this.yPos +=3;
-                this.xPos +=2.5;
+                case 6: this.yPos +=3 * this.moveScale;
+                    this.xPos +=2.5 * this.moveScale;
                     break;
-                case 7: this.yPos +=0;
+                case 7: this.yPos +=0 * this.moveScale;
                     break;
-                case 8: this.yPos +=0;
+                case 8: this.yPos +=0 * this.moveScale;
                     break;
             }
             this.tick = 0;
@@ -48,6 +49,6 @@ class Miku {
     }
 
     draw(ctx) {
-        this.jump.drawFrame(this.game.clockTick, ctx, this.xPos, this.yPos, 3);
+        this.jump.drawFrame(this.game.clockTick, ctx, this.xPos, this.yPos, 4);
     }
 }
