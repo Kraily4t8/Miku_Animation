a98a127ad2d47bd6792cc4d0e9b05f859a3e02a5
Kraily4t8 <53635947+Kraily4t8@users.noreply.github.com>
2024-01-05 02:26:47 -0800

    jump to walk

--- miku.js
+++ miku.js
@@ -4,51 +4,60 @@ class Miku {
         //(spritesheet, xStart, yStart, refWidth, refHeight, frameCount, frameDuration, framePadding, reverse, loop)
         this.idle = new Animator(this.spritesheet, 2, 1, 56, 64, 10, 0.2, 3, false, true);
         this.walkRight = new Animator(this.spritesheet, 2, 65, 75, 67, 8, 0.2, 2, false, true);
-
         this.jump = new Animator(this.spritesheet, 5, 134, 75, 100, 9, 0.23, 2.25, false, true);
 
+        this.currentAnimator = this.jump;
+        this.tickSum = 0;
         this.tick = 0;
-        this.xPos = 400;
+        this.xPos = -40;
         this.yPos = 250;
-        this.moveScale = 1.5;
+        this.moveScale = 0.5;
         //loop length
     }
 
     update() {
-        // determine amount of time of the start up jump
         console.log(this.jump.currentFrame());
         this.tick++;
+        this.tickSum++;
         if(this.xPos > 960) this.xPos = -240;
         if(this.tick > 2) {
-            switch(this.jump.currentFrame()) {
-                case 1: this.yPos -=2 * this.moveScale;
-                    this.xPos +=2 * this.moveScale;
-                    break;
-                case 2: this.yPos -=2 * this.moveScale;
-                    this.xPos +=2.5 * this.moveScale;
-                    break;
-                case 3: this.yPos -=2 * this.moveScale;
-                    this.xPos +=2.5 * this.moveScale;
-                    break;
-                case 4: this.yPos -=0 * this.moveScale;
-                    this.xPos +=2.5 * this.moveScale;
-                    break;
-                case 5: this.yPos +=3 * this.moveScale;
-                    this.xPos +=2.5 * this.moveScale;
-                    break;
-                case 6: this.yPos +=3 * this.moveScale;
-                    this.xPos +=2.5 * this.moveScale;
-                    break;
-                case 7: this.yPos +=0 * this.moveScale;
-                    break;
-                case 8: this.yPos +=0 * this.moveScale;
-                    break;
+            if(this.tickSum < 360) {
+                switch(this.jump.currentFrame()) {
+                    case 1: this.yPos -=2 * this.moveScale;
+                        this.xPos +=2 * this.moveScale;
+                        break;
+                    case 2: this.yPos -=2 * this.moveScale;
+                        this.xPos +=2.5 * this.moveScale;
+                        break;
+                    case 3: this.yPos -=2 * this.moveScale;
+                        this.xPos +=2.5 * this.moveScale;
+                        break;
+                    case 4: this.yPos -=0 * this.moveScale;
+                        this.xPos +=2.5 * this.moveScale;
+                        break;
+                    case 5: this.yPos +=3 * this.moveScale;
+                        this.xPos +=2.5 * this.moveScale;
+                        break;
+                    case 6: this.yPos +=3 * this.moveScale;
+                        this.xPos +=2.5 * this.moveScale;
+                        break;
+                    case 7: this.yPos +=0 * this.moveScale;
+                        break;
+                    case 8: this.yPos +=0 * this.moveScale;
+                        break;
+                }
+            } else {
+                this.currentAnimator = this.walkRight;
+                this.xPos++;
             }
-            this.tick = 0;
         }
     }
 
     draw(ctx) {
-        this.jump.drawFrame(this.game.clockTick, ctx, this.xPos, this.yPos, 4);
+        if(this.currentAnimator == this.jump) {
+            this.currentAnimator.drawFrame(this.game.clockTick, ctx, this.xPos, this.yPos, 3);
+        } else if (this.currentAnimator == this.walkRight) {
+            this.currentAnimator.drawFrame(this.game.clockTick, ctx, this.xPos-10, this.yPos + 100, 3);
+        }
     }
 }
