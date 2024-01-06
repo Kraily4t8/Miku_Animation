fe8890fe4ae98f6f8ccfdd055089acc37f61d5d5
Kraily4t8 <53635947+Kraily4t8@users.noreply.github.com>
2024-01-06 00:17:43 -0800

    cleaned up sprite

--- miku.js
+++ miku.js
@@ -2,62 +2,31 @@ class Miku {
     constructor(game, x, y, spritesheet) {
         Object.assign(this, { game, x, y, spritesheet});
         //(spritesheet, xStart, yStart, refWidth, refHeight, frameCount, frameDuration, framePadding, reverse, loop)
-        this.idle = new Animator(this.spritesheet, 2, 1, 56, 64, 10, 0.2, 3, false, true);
+        this.idle = new Animator(this.spritesheet, 0, 1, 59, 64, 10, 0.2, 0, false, true);
         this.walkRight = new Animator(this.spritesheet, 2, 65, 75, 67, 8, 0.2, 2, false, true);
-        this.jump = new Animator(this.spritesheet, 5, 134, 75, 100, 9, 0.23, 2.25, false, true);
-
-        this.currentAnimator = this.jump;
-        this.tickSum = 0;
-        this.tick = 0;
-        this.xPos = -40;
-        this.yPos = 250;
-        this.moveScale = 0.5;
+        this.jumping = new Animator(this.spritesheet, 1, 134, 74, 100, 9, 0.23, 4, false, true);
+        this.jump = new Animator(this.spritesheet, 1, 134, 74, 100, 5, 0.23, 4, false, true);
+        this.falling = new Animator(this.spritesheet, 395, 134, 75, 100, 2, 0.23, 2.25, false, true);
+        this.landing = new Animator(this.spritesheet, 552, 134, 75, 100, 2, 0.23, 2.25, false, true);
+        this.attack = new Animator(this.spritesheet, 5, 236, 82, 65, 8, 0.2, 2.25, false, true);
+        this.defeat = new Animator(this.spritesheet, 0, 370, 86, 60, 3, 0.6, 0, false, true);
+        this.dance = new Animator(this.spritesheet, 0, 432, 83.5, 64, 12, 0.25, 0.45, false, true);
         //loop length
     }
 
     update() {
-        console.log(this.jump.currentFrame());
-        this.tick++;
-        this.tickSum++;
-        if(this.xPos > 960) this.xPos = -240;
-        if(this.tick > 2) {
-            if(this.tickSum < 360) {
-                switch(this.jump.currentFrame()) {
-                    case 1: this.yPos -=2 * this.moveScale;
-                        this.xPos +=2 * this.moveScale;
-                        break;
-                    case 2: this.yPos -=2 * this.moveScale;
-                        this.xPos +=2.5 * this.moveScale;
-                        break;
-                    case 3: this.yPos -=2 * this.moveScale;
-                        this.xPos +=2.5 * this.moveScale;
-                        break;
-                    case 4: this.yPos -=0 * this.moveScale;
-                        this.xPos +=2.5 * this.moveScale;
-                        break;
-                    case 5: this.yPos +=3 * this.moveScale;
-                        this.xPos +=2.5 * this.moveScale;
-                        break;
-                    case 6: this.yPos +=3 * this.moveScale;
-                        this.xPos +=2.5 * this.moveScale;
-                        break;
-                    case 7: this.yPos +=0 * this.moveScale;
-                        break;
-                    case 8: this.yPos +=0 * this.moveScale;
-                        break;
-                }
-            } else {
-                this.currentAnimator = this.walkRight;
-                this.xPos++;
-            }
-        }
+        
     }
 
     draw(ctx) {
-        if(this.currentAnimator == this.jump) {
-            this.currentAnimator.drawFrame(this.game.clockTick, ctx, this.xPos, this.yPos, 3);
-        } else if (this.currentAnimator == this.walkRight) {
-            this.currentAnimator.drawFrame(this.game.clockTick, ctx, this.xPos-10, this.yPos + 100, 3);
-        }
+        this.idle.drawFrame(this.game.clockTick, ctx, 0, 0, 3);
+        this.walkRight.drawFrame(this.game.clockTick, ctx, 180, 0, 3);
+        this.jumping.drawFrame(this.game.clockTick, ctx, 0, 200, 3);
+        this.jump.drawFrame(this.game.clockTick, ctx, 210, 200, 3);
+        this.falling.drawFrame(this.game.clockTick, ctx, 400, 200, 3);
+        this.landing.drawFrame(this.game.clockTick, ctx, 600, 200, 3);
+        this.attack.drawFrame(this.game.clockTick, ctx, 400, 0, 3);
+        this.defeat.drawFrame(this.game.clockTick, ctx, 0, 500, 3);
+        this.dance.drawFrame(this.game.clockTick, ctx, 250, 500, 3);
     }
 }
